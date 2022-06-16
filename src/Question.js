d60f71c38b66bc30986c6f13fb4ce9e517c0d80c
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-06-16 02:34:33 -0700

    question component setup

--- src/Question.js
+++ src/Question.js
@@ -1,7 +1,42 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
-const Question = () => {
-  return <h2>question component</h2>;
+const Question = ({ arrData, handlePlusClick, passedID }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  function handleAddClick() {
+    setIsShown(!isShown);
+  }
+
+  function handleMinusClick() {
+    setIsShown(!isShown);
+  }
+  return (
+    <article>
+      {arrData.map((obj, index) => {
+        const { id, title, info } = obj;
+        return (
+          <div className="question">
+            <header>
+              <h4>{title} </h4>
+              <span className="btn">
+                {!isShown ? (
+                  <AiOutlinePlus
+                    onClick={() => {
+                      handleAddClick();
+                      handlePlusClick(id);
+                    }}
+                  />
+                ) : (
+                  <AiOutlineMinus onClick={handleMinusClick} />
+                )}
+              </span>
+            </header>
+            {index === passedID && isShown ? <p>{info}</p> : null}
+          </div>
+        );
+      })}
+    </article>
+  );
 };
 
 export default Question;
