7ce32e83de06a7107d25c26011d8ddbc7d7cf246
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-06-16 02:35:00 -0700

    handling arr mapping through component prop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React, { useState } from "react";
 import data from "./data";
 import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [myNum, setMyNum] = useState();
+  function handlePlusClick(id) {
+    let myNewID = id - 1;
+    setMyNum(myNewID);
+    console.log(myNewID);
+
+    // setIsShown(!isShown);
+  }
+  return (
+    <main>
+      <section className="container">
+        <h3>Questions And Answers About Login</h3>
+        <SingleQuestion
+          arrData={data}
+          handlePlusClick={handlePlusClick}
+          passedID={myNum}
+        />
+      </section>
+    </main>
+  );
 }
 
 export default App;
